cd265dbee7bebc7d554e4d4ace79a61060883c46
SowmiyaNarayanan123 <snnarayanan195@gmail.com>
2025-05-13 10:09:34 +0530

    Update WaterSubMeterAggregationRepository.java

--- src/main/java/com/productioncheck/repository/WaterSubMeterAggregationRepository.java
+++ src/main/java/com/productioncheck/repository/WaterSubMeterAggregationRepository.java
@@ -3,7 +3,6 @@ package com.productioncheck.repository;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -11,7 +10,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -25,7 +23,6 @@ public class WaterSubMeterAggregationRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
-
     public Page<WaterSubMeter> findFilteredData(
             List<String> colony,
             List<String> community,
@@ -60,20 +57,13 @@ public class WaterSubMeterAggregationRepository {
         if (!CollectionUtils.isEmpty(site)) {
             predicates.add(root.get("site").in(site));
         }
-
         cq.select(root).where(predicates.toArray(new Predicate[0]));
-
-      
         Query countQuery = entityManager.createQuery(cq);
         int totalRecords = countQuery.getResultList().size();
-
-       
         Query query = entityManager.createQuery(cq);
         query.setFirstResult((int) pageable.getOffset());
         query.setMaxResults(pageable.getPageSize());
-
-        List<WaterSubMeter> resultList = query.getResultList();
-
+       List<WaterSubMeter> resultList = query.getResultList();
         return new PageImpl<>(resultList, pageable, totalRecords);
     }
 }
