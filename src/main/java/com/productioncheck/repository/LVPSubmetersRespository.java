8c87136d23ab6b6dcd06d20d2bcf10accbe7719e
SowmiyaNarayanan123 <snnarayanan195@gmail.com>
2025-05-13 10:06:06 +0530

    Update LVPSubmetersRespository.java

--- src/main/java/com/productioncheck/repository/LVPSubmetersRespository.java
+++ src/main/java/com/productioncheck/repository/LVPSubmetersRespository.java
@@ -1,9 +1,7 @@
 package com.productioncheck.repository;
-
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -11,13 +9,11 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.CollectionUtils;
-
 import com.productioncheck.beans.LVPSubMeter;
 
 @Repository
@@ -44,11 +40,9 @@ public class LVPSubmetersRespository {
         if (startTimestamp != null && endTimestamp != null) {
             predicates.add(cb.between(root.get("datetime"), startTimestamp, endTimestamp));
         }
-
         if (!CollectionUtils.isEmpty(colony)) {
             predicates.add(root.get("colony").in(colony));
         }
-
         if (!CollectionUtils.isEmpty(community)) {
             predicates.add(root.get("community").in(community));
         }
